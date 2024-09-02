1147370da40761756982047f854c0cf82ff44c45
xiangxiongfly <2278779027@qq.com>
2024-09-02 14:57:47 +0800

    update

--- uniapp_video/uniapp_video/pages/detail/detail.vue
+++ uniapp_video/uniapp_video/pages/detail/detail.vue
@@ -1,22 +1,241 @@
-<template>
-	<view>
-		
-	</view>
-</template>
+  <style lang="scss">
+  	.video-info {
+  		margin-top: 10rpx;
 
-<script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
-</script>
+  		.video {
+  			width: 100%;
+  		}
 
-<style>
+  		.video-title {
+  			display: flex;
+  			justify-content: space-between;
+  			font-size: 35rpx;
+  		}
 
-</style>
+  		.video-detail {
+  			margin-top: 5rpx;
+  			font-size: 28rpx;
+
+  			text {
+  				margin-left: 15rpx;
+  			}
+
+  			.author {
+  				color: #000;
+  			}
+  		}
+  	}
+
+  	.other-list {
+  		margin-top: 10rpx;
+
+  		.item-other {
+  			display: flex;
+  			justify-content: space-between;
+  			margin-bottom: 20rpx;
+
+  			.other-img-wrap {
+  				width: 30%;
+  				display: flex;
+  				justify-content: center;
+  				align-items: center;
+
+  				image {
+  					width: 90%;
+  					border-radius: 15rpx[];
+  				}
+  			}
+
+  			.other-info {
+  				width: 60%;
+  				display: flex;
+  				flex-direction: column;
+  				justify-content: space-around;
+
+  				.other-title {
+  					font-size: 30rpx;
+  					color: #e06f93;
+  				}
+
+  				.other-detail {
+  					font-size: 26rpx;
+  					color: #666;
+  				}
+  			}
+  		}
+  	}
+
+  	.comment-wrap {
+  		margin-top: 10rpx;
+
+  		.comment-title {
+  			padding: 10rpx;
+  			font-size: 28rpx;
+  		}
+
+  		.comment-list {
+  			.comment-item {
+  				margin-bottom: 10rpx;
+  				padding: 10rpx;
+  				display: flex;
+  				justify-content: space-between;
+  				border-bottom: 1px solid #666;
+  			}
+
+  			.comment-user {
+  				flex: 1;
+  				display: flex;
+  				justify-content: center;
+  				align-items: center;
+
+  				.comment-user-image {
+  					width: 60%;
+  					border-radius: 50%;
+  				}
+  			}
+
+  			.comment-info {
+  				flex: 5;
+  				display: flex;
+  				flex-direction: column;
+  				justify-content: space-around;
+
+  				.comment-detail {
+  					display: flex;
+  					justify-content: space-between;
+
+  					.author {
+  						font-size: 28rpx;
+  						color: #000
+  					}
+
+  					.date {
+  						color: #666;
+  						font-size: 24rpx;
+  					}
+  				}
+
+  				.comment-content {
+  					font-size: 28rpx;
+  					color: #000;
+  				}
+  			}
+  		}
+  	}
+  </style>
+
+  <template>
+  	<view>
+  		<MyTitle></MyTitle>
+  		<!-- 视频详情 -->
+  		<view class="video-info" v-if="videoInfo">
+  			<video :src="videoInfo.videoSrc" controls class="video"></video>
+  			<view class="video-title">
+  				<text>{{videoInfo.videoTitle}}</text>
+  				<text class="fa fa-angle-down"></text>
+  			</view>
+  			<view class="video-detail">
+  				<text class="author">{{videoInfo.author}}</text>
+  				<text class="play-count">{{videoInfo.playCount}}</text>
+  				<text class="comment-count">{{videoInfo.commentCount}}弹幕</text>
+  				<text class="date">时间：{{videoInfo.date}}</text>
+  			</view>
+  		</view>
+  		<!-- 推荐视频 -->
+  		<view class="other-list">
+  			<view class="item-other" v-for="item in otherList" :key="item.id">
+  				<view class="other-img-wrap">
+  					<image :src="item.imgSrc" mode="widthFix"></image>
+  				</view>
+  				<view class="other-info">
+  					<view class="other-title">{{item.title}}</view>
+  					<view class="other-detail">
+  						<text class="play-count">{{item.playMsg}}次观看</text>
+  						<text class="comment-count">{{item.commentCount}}条评论</text>
+  					</view>
+  				</view>
+  			</view>
+  		</view>
+  		<!-- 评论列表 -->
+  		<view class="comment-wrap">
+  			<view class="comment-title">
+  				评论{{commentData.commentTotalCount}}
+  			</view>
+  			<view class="comment-list">
+  				<view class="comment-item" v-for="(item,index) in commentData.commentList" :key="index">
+  					<view class="comment-user">
+  						<image :src="item.userIconSrc" mode="widthFix" class="comment-user-image"></image>
+  					</view>
+  					<view class="comment-info">
+  						<view class="comment-detail">
+  							<text class="author">{{item.username}}</text>
+  							<text class="date">{{item.commentDate}}</text>
+  						</view>
+  						<view class="comment-content">
+  							{{item.commentInfo}}
+  						</view>
+  					</view>
+  				</view>
+  			</view>
+  		</view>
+  	</view>
+  </template>
+
+  <script>
+  	import config from "../../common/config.js"
+  	export default {
+  		data() {
+  			return {
+  				videoInfo: null,
+  				otherList: [],
+  				commentData: null
+  			}
+  		},
+  		onLoad(options) {
+  			const videoId = options.id
+  			this.getCurentVideo(videoId)
+  			this.getOtherList()
+  			this.getCommentList(videoId)
+  		},
+  		methods: {
+  			getCurentVideo(videoId) {
+  				uni.request({
+  					url: config.url + "/videoDetail?id=" + videoId,
+  					success: res => {
+  						const result = res.data
+  						// console.log(result);
+  						if (result.code === 0) {
+  							this.videoInfo = result.data.videoInfo
+  						}
+  					}
+  				})
+  			},
+  			getOtherList() {
+  				console.log(config.url + "/othersList");
+  				uni.request({
+  					url: config.url + "/othersList",
+  					success: res => {
+  						const result = res.data
+  						// console.log(result);
+  						if (result.code === 0) {
+  							this.otherList = result.data.othersList
+  						}
+  					}
+  				})
+  			},
+  			getCommentList(videoId) {
+  				console.log(config.url + "/commentsList?id=" + videoId);
+  				uni.request({
+  					url: config.url + "/commentsList?id=" + videoId,
+  					success: res => {
+  						const result = res.data
+  						console.log(result);
+  						if (result.code === 0) {
+  							this.commentData = result.data.commentData
+  						}
+  					}
+  				})
+  			}
+  		}
+  	}
+  </script>
--- uniapp_video/uniapp_video/pages/index/index.vue
+++ uniapp_video/uniapp_video/pages/index/index.vue
@@ -1,67 +1,3 @@
-<template>
-	<view>
-		<MyTitle></MyTitle>
-		<!-- 导航栏 -->
-		<view class="nav_wrap">
-			<scroll-view class="nav" scroll-x>
-				<view @click="activeNav($event,index)" :class="['nav_item',index===currentIndexNav?'active':'']"
-					:data-index="index" v-for="(item,index) in navList" :key="item.id">
-					{{item.text}}
-				</view>
-			</scroll-view>
-		</view>
-		<!-- 轮播图 -->
-		<view class="slides">
-			<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
-				<swiper-item v-for="(item,index) in swiperList" :key="index">
-					<image :src="item.imgSrc" mode="widthFix"></image>
-				</swiper-item>
-			</swiper>
-		</view>
-	</view>
-</template>
-
-<script>
-	// import config from "../../common/config.js"
-	const config = require("../../common/config.js")
-	export default {
-		data() {
-			return {
-				navList: [],
-				currentIndexNav: 0,
-				swiperList: [],
-				videoList: []
-			}
-		},
-		onLoad() {
-			this.getNavList()
-			this.getSwiperList()
-		},
-		methods: {
-			getNavList: function() {
-				uni.request({
-					url: config.url + "/navList",
-					success: res => {
-						this.navList = res.data.data.navList
-					}
-				})
-			},
-			activeNav: function(e, index) {
-				// this.currentIndexNav = e.target.dataset.index
-				this.currentIndexNav = index
-			},
-			getSwiperList: function() {
-				uni.request({
-					url: config.url + "/swiperList",
-					success: res => {
-						this.swiperList = res.data.data.swiperList
-					}
-				})
-			}
-		}
-	}
-</script>
-
 <style lang="scss">
 	.nav {
 		white-space: nowrap;
@@ -90,4 +26,156 @@
 			}
 		}
 	}
+
+	.video-wrap {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		padding: 5rpx;
+	}
+
+	.video-item {
+		width: 48%;
+		margin-bottom: 20rpx;
+	}
+
+	.video-img {
+		position: relative;
+
+		image {
+			width: 100%;
+			border-radius: 15rpx;
+		}
+
+		.video-info {
+			position: absolute;
+			left: 0;
+			bottom: 8rpx;
+			width: 100%;
+			display: flex;
+			justify-content: space-around;
+			background-color: rgba(0, 0, 0, 0.4);
+
+			text {
+				margin-right: 5rpx;
+				font-size: 24rpx;
+				color: #efefef;
+			}
+		}
+	}
+
+	.video-title {
+		font-size: 28rpx;
+		/* 将对象作为弹性伸缩盒子模型显示 */
+		display: -webkit-box;
+		/* 超出部分隐藏 */
+		overflow: hidden;
+		/* 显示的行数 */
+		-webkit-line-clamp: 2;
+		/* 从上到下垂直排列子元素 */
+		-webkit-box-orient: vertical;
+	}
 </style>
+
+<template>
+	<view>
+		<MyTitle></MyTitle>
+		<!-- 导航栏 -->
+		<view class="nav_wrap">
+			<scroll-view class="nav" scroll-x>
+				<view @click="activeNav($event,index)" :class="['nav_item',index===currentIndexNav?'active':'']"
+					:data-index="index" v-for="(item,index) in navList" :key="item.id">
+					{{item.text}}
+				</view>
+			</scroll-view>
+		</view>
+		<!-- 轮播图 -->
+		<view class="slides">
+			<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+				<swiper-item v-for="(item,index) in swiperList" :key="index">
+					<image :src="item.imgSrc" mode="widthFix"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<!-- 视频列表 -->
+		<view class="video-wrap">
+			<view class="video-item" @click="toVideoDetail(item.id)" v-for="(item,index) in videoList" :key="item.id">
+				<view class="video-img">
+					<image :src="item.imgSrc" mode="widthFix"></image>
+					<view class="video-info">
+						<!-- 播放量 -->
+						<view class="play-count-wrap">
+							<text class="fa fa-play-circle-o"></text>
+							<text class="play-count">{{item.playCount}}</text>
+						</view>
+						<!-- 评论量 -->
+						<view class="comment-count-row">
+							<text class="fa fa-commenting-o"></text>
+							<text class="comment-count">{{item.commentCount}}</text>
+						</view>
+					</view>
+				</view>
+				<view class="video-title">
+					{{item.desc}}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import config from "../../common/config.js"
+	// const config = require("../../common/config.js")
+
+	export default {
+		data() {
+			return {
+				navList: [],
+				currentIndexNav: 0,
+				swiperList: [],
+				videoList: []
+			}
+		},
+		onLoad() {
+			this.getNavList()
+			this.getSwiperList()
+			this.getVideoList()
+		},
+		methods: {
+			getNavList: function() {
+				uni.request({
+					url: config.url + "/navList",
+					success: res => {
+						this.navList = res.data.data.navList
+					}
+				})
+			},
+			activeNav: function(e, index) {
+				// this.currentIndexNav = e.target.dataset.index
+				this.currentIndexNav = index
+			},
+			getSwiperList: function() {
+				uni.request({
+					url: config.url + "/swiperList",
+					success: res => {
+						this.swiperList = res.data.data.swiperList
+					}
+				})
+			},
+			getVideoList: function() {
+				uni.request({
+					url: config.url + "/videosList",
+					success: res => {
+						const result = res.data
+						this.videoList = result.data.videosList
+					}
+				})
+			},
+			toVideoDetail: function(id) {
+				uni.navigateTo({
+					url: "/pages/detail/detail?id=" + id
+				})
+			}
+		}
+	}
+</script>
